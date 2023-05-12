43f7372fe86ccd01215ff0f004d2d68aa4dbd798
Shaxzod <painloversgame@gmail.com>
2023-05-12 18:21:39 +0500

    APIView CRUD

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
-from .views import CategoryAPIView, ProductAPIView, BannerAPIView
+from .views import CategoryAPIView, ProductAPIView, BannerAPIView, ProductDetail, CategoriesDetail
 
 
 urlpatterns = [
     path('categories/', CategoryAPIView.as_view()),
     path('products/', ProductAPIView.as_view()),
     path('banners/', BannerAPIView.as_view()),
+    path('products/<int:pk>/', ProductDetail.as_view()),
+    path('categories/<int:pk>/', CategoriesDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
+from rest_framework import status
 from products.models import Category, Product, Banner
 from .serializers import CategorySerializer, ProductSerializer, BannerSerializer
 
@@ -10,13 +12,94 @@ class CategoryAPIView(APIView):
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoriesDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(id=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        categories = self.get_object(pk)
+        serializer = CategorySerializer(categories)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        categories = self.get_object(pk)
+        serializer = CategorySerializer(categories, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        categories = self.get_object(pk)
+        serializer = CategorySerializer(categories, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        categories = self.get_object(pk)
+        categories.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class ProductAPIView(APIView):
     def get(self, request):
         products = Product.objects.all()
-        serializer = ProductSerializer(
-            products, context={'request': request}, many=True)
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(id=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class BannerAPIView(APIView):
